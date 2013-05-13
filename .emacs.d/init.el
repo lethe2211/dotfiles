f372ea23c55ff21bc3fbb480455a43c8b41c0f39
lethe-lab <lethe-lab@lethelab-virtual-machine.(none)>
2013-05-13 12:38:29 +0900

    Dotfiles 20130525

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -38,17 +38,6 @@
 (setq x-select-enable-clipboard t)
 (global-set-key "\C-y" 'x-clipboard-yank)
 
-;端末版EmacsとX-Windowとのクリップボード共有(xselのインストールが必要)
-;; (setq interprogram-paste-function
-;;       (lambda ()
-;;          (shell-command-to-string "xsel -b -o")))
-;; (setq interprogram-cut-function
-;;       (lambda (text &optional rest)
-;;          (let* ((process-connection-type nil)
-;;                 (proc (start-process "xsel" "*Messages*" "xsel" "-b" "-i")))
-;;            (process-send-string proc text)
-;;            (process-send-eof proc))))
-
 ;autoinsert
 ;テンプレートは~/emacs.d/templateに置く
 (setq auto-insert-directory "~/.emacs.d/template/")
