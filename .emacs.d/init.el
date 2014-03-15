a247516cf4467da4be3bddf04ba9304238a5b622
lethe <lethe2211@gmail.com>
2014-03-15 16:34:12 +0900

    change flymake settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -61,26 +61,26 @@
              (flymake-mode t)))
 
 ;Python
-(defun flymake-python-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-inplace))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    (list "pyflakes" (list local-file))))
+;; (defun flymake-python-init ()
+;;   (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                      'flymake-create-temp-inplace))
+;;          (local-file (file-relative-name
+;;                       temp-file
+;;                       (file-name-directory buffer-file-name))))
+;;     (list "pyflakes" (list local-file))))
  
-(defconst flymake-allowed-python-file-name-masks '(("\.py$" flymake-python-init)))
-(defvar flymake-python-err-line-patterns '(("\(.*\):\([0-9]+\):\(.*\)" 1 2 nil 3)))
+;; (defconst flymake-allowed-python-file-name-masks '(("\.py$" flymake-python-init)))
+;; (defvar flymake-python-err-line-patterns '(("\(.*\):\([0-9]+\):\(.*\)" 1 2 nil 3)))
  
-(defun flymake-python-load ()
-  (interactive)
-  (defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
-    (setq flymake-check-was-interrupted t))
-  (ad-activate 'flymake-post-syntax-check)
-  (setq flymake-allowed-file-name-masks (append flymake-allowed-file-name-masks flymake-allowed-python-file-name-masks))
-  (setq flymake-err-line-patterns flymake-python-err-line-patterns)
-  (flymake-mode t))
-(add-hook 'python-mode-hook '(lambda () (flymake-python-load)))
+;; (defun flymake-python-load ()
+;;   (interactive)
+;;   (defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
+;;     (setq flymake-check-was-interrupted t))
+;;   (ad-activate 'flymake-post-syntax-check)
+;;   (setq flymake-allowed-file-name-masks (append flymake-allowed-file-name-masks flymake-allowed-python-file-name-masks))
+;;   (setq flymake-err-line-patterns flymake-python-err-line-patterns)
+;;   (flymake-mode t))
+;; (add-hook 'python-mode-hook '(lambda () (flymake-python-load)))
 
 ;C-hでカーソル前の1文字を消す(BackSpace)
 (global-set-key "\C-h" 'delete-backward-char)
