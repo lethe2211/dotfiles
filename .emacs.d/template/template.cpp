10bee7c1e82f095249e37aeeea932e34e7042c90
lethe <lethe2211@gmail.com>
2014-03-31 12:17:26 +0900

    Change template

---
+++ .emacs.d/template/template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main() {
+
+
+  
+  return 0;
+
+}
