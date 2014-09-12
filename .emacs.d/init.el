528232757e0fa913bab17b9298f3f153f5f5cc92
lethe <lethe2211@gmail.com>
2014-09-12 18:48:39 +0900

    bug fixed

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,10 +24,43 @@
 
 ;;; Code:
 
+(defun x->bool (elt) (not (not elt)))
+
+;; emacs-version predicates
+(setq emacs22-p (string-match "^22" emacs-version)
+      emacs23-p (string-match "^23" emacs-version)
+      emacs23.0-p (string-match "^23\.0" emacs-version)
+      emacs23.1-p (string-match "^23\.1" emacs-version)
+      emacs23.2-p (string-match "^23\.2" emacs-version)
+      emacs24 (string-match "^24" emacs-version))
+
+;; system-type predicates
+(setq darwin-p  (eq system-type 'darwin)
+      ns-p      (eq window-system 'ns)
+      carbon-p  (eq window-system 'mac)
+      linux-p   (eq system-type 'gnu/linux)
+      colinux-p (when linux-p
+                  (let ((file "/proc/modules"))
+                    (and
+                     (file-readable-p file)
+                     (x->bool
+                      (with-temp-buffer
+                        (insert-file-contents file)
+                        (goto-char (point-min))
+                        (re-search-forward "^cofuse\.+" nil t))))))
+      cygwin-p  (eq system-type 'cygwin)
+      nt-p      (eq system-type 'windows-nt)
+      meadow-p  (featurep 'meadow)
+      windows-p (or cygwin-p nt-p meadow-p))
 
 ;; caskの設定
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
+(when darwin-p
+  (require 'cask )
+  (cask-initialize))
+
+(when linux-p
+  (require 'cask "~/.cask/cask.el")
+  (cask-initialize))
 
 ;; init-loaderによるファイルの分割読み込み
 (require 'init-loader)
---
+++ .emacs.d/init_loader/00_global-conf.el
@@ -0,0 +1,29 @@
+;;; 00_global-conf.el --- 
+
+;; Copyright (C) 2014  admin
+
+;; Author: admin <admin@h77.26.238.10.30790.vlan.kuins.net>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 全体的な設定
+
+;;; Code:
+
+
+(provide '00_global-conf)
+;;; 00_global-conf.el ends here
