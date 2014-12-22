71d35d78264c2dc0405b63ed3df68bc077c906eb
lethe <lethe2211@gmail.com>
2014-12-22 23:14:24 +0900

    remove files that are children of .cask and .python-environments

--- .emacs.d/.cask/24.3.1/bootstrap/git-20140128.241/git-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; git-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("../../../../../../.emacs.d/.cask/24.3.1/bootstrap/git-20140128.241/git-pkg.el"
-;;;;;;  "../../../../../../.emacs.d/.cask/24.3.1/bootstrap/git-20140128.241/git.el")
-;;;;;;  (21522 50969 943821 82000))
-
-;;;***
-
-(provide 'git-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; git-autoloads.el ends here
--- .emacs.d/.cask/24.3.1/elpa/flycheck-cask-20140814.116/flycheck-cask-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "flycheck-cask" "20140814.116" "Cask support in Flycheck" (quote ((emacs "24.1") (flycheck "0.14") (dash "2.4.0"))))
