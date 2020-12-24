4609ea27642d97b5538a911550f015b883aad3ad
Shogen, Shuhei | Lethe | ELPD <shuhei.shogen@rakuten.com>
2020-12-24 15:33:46 +0900

    Add a shell script to set it up

---
+++ setup.sh
@@ -0,0 +1,16 @@
+# Switch shell to zsh
+chsh -s /bin/zsh
+
+# zsh
+ln -s ~/dotfiles/.zlogin ~/.zlogin
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zenv ~/.zenv
+
+# Git
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
+# tmux
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# Emacs
+ln -s ~/dotfiles/.emacs.d ~/.emacs.d
